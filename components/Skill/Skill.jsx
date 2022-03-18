d55f016534ba4e7141d14b8bab70b6336d5b43cb
Vinicius Goneli <viniciusgoneli_2012@hotmail.com>
2022-03-18 08:09:52 -0300

    Created and implemented 'Skill.module.css'

--- components/Skill/Skill.jsx
+++ components/Skill/Skill.jsx
@@ -1,14 +1,15 @@
 import Image from 'next/image'
+import styles from './Skill.module.css'
 
 export default function Skill(props){
     return(
-        <section>
-            <div>
-                <Image src={props.skillImg} width='125px' height='125px' />
-                <h2>{props.skillTitle}</h2>
+        <section className={styles.skill}>
+            <div className={styles.skill__imageDiv}>
+                <Image className={styles.imageDiv__image} src={props.skillImg} width='125px' height='125px' />
+                <h2 className={styles.imageDiv__title}>{props.skillTitle}</h2>
             </div>
-            <div>
-                <ul>
+            <div className={styles.skill__listDiv}>
+                <ul className={styles.listDiv__list}>
                 </ul>
             </div>
         </section>
