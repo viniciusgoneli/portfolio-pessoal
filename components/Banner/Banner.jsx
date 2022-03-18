55219f08d03d348e94e4eca86017950d053e2195
Vinicius Goneli <viniciusgoneli_2012@hotmail.com>
2022-03-17 23:36:53 -0300

    Created and implemented 'Banner.module.css'

--- components/Banner/Banner.jsx
+++ components/Banner/Banner.jsx
@@ -1,19 +1,20 @@
 import bannerImage from './../../public/images/banner-image.png'
 import Image from 'next/image'
+import styles from './Banner.module.css'
 
 function Banner(){
     const description = 'Meu nome é Vinicius, tenho 24 anos e sou desenvolvedor front-end com 6 meses de experiência na área. Eu amo resolver problemas usando programação a tal ponto de passar o dia todo estudando e me aprimorando.'
     return(
-        <section>
-            <div>
-                <div>
-                    <h1>Vinicius Goneli</h1>
-                    <h2>Desenvolvedor front-end</h2>
-                    <p>{description}</p>
+        <section className={styles.banner}>
+            <div className={styles.banner__titleDescriptionDiv}>
+                <div className={styles.titleDescriptionDiv__container}>
+                    <h1 className={styles.titleDescriptionDiv__title}>Vinicius Goneli</h1>
+                    <h2 className={styles.titleDescriptionDiv__subtitle}>Desenvolvedor front-end</h2>
+                    <p className={styles.titleDescriptionDiv__description}>{description}</p>
                 </div>
             </div>
-            <div>
-                <Image draggable={false} src={bannerImage} />
+            <div className={styles.banner__imgDiv}>
+                <Image draggable={false} src={bannerImage} width='300px' height='600px' />
             </div>
         </section>
     )
